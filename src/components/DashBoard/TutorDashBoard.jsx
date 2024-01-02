d692592f6e7b4a57cc5487084ddbaefe2df57c7f
codewitHK1 <kumarhk1002@gmail.com>
2024-01-02 09:32:27 +0530

    fix deployment issue

--- src/components/DashBoard/TutorDashBoard.jsx
+++ src/components/DashBoard/TutorDashBoard.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import SwipeableTemporaryDrawer from "../SideDrawer/Drawer";
 
 const TutorDashBoard = () => {
   return (
     <div>
       <h1>TutorDashboard</h1>
-      <SwipeableTemporaryDrawer />
     </div>
   );
 };
